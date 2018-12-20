11eb094cc52fcd7f2551f64f25d37611abb05248
billingsby <mark@markbillingsby.com>
2018-12-20 11:38:03 -0700

    testing IG feed

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-const showInsta = function(data) {
+var showInsta = function(data) {
   
  
-  let thumbnail = data.images.standard_resolution.url;
-  let media;
-  let link = data.link;
-  let caption = data.caption.text;
-  let result; 
+  var thumbnail = data.images.standard_resolution.url;
+  var media;
+  var link = data.link;
+  var caption = data.caption.text;
+  var result; 
 
   if (data.type == 'image') {
     media = data.images.standard_resolution.url;
@@ -19,37 +19,53 @@ const showInsta = function(data) {
 };
 
 function getInsta() {
-  
-  var parameters = { 
-    access_token: '14109436.17e910a.4e61a5540a2240d88efc17a2a3232107',
-    count: 12
-  };
-  
-  $.ajax({
-    url: 'https://api.instagram.com/v1/users/self/media/recent/',
-    data: parameters,
-    dataType: 'jsonp',
-    type: 'GET'
-  })
-  .done(function(result){ 
-    $.each(result.data, function(i, data) {
-      var instaPhoto = showInsta(data);
-      
-      if (i < 4) {
-        $('#insta-items').append(instaPhoto);
-      } else if (i < 8) {
-        $('#insta-items-2').append(instaPhoto);
-      } else if (i < 12) {
-        $('#insta-items-3').append(instaPhoto);
-      }
 
+  var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'awesome',
+        clientId: 'e7dc08a48f4340258d16fef5ebf5f836'
     });
+    feed.run();
+  
+
+  // var userFeed = new Instafeed({
+  //   get: 'user',
+  //   userId: '14109436',
+  //   accessToken: '14109436.1677ed0.6766c773b72749588fcdefa86020f837'
+  // });
+  // userFeed.run();
+
+  // var parameters = { 
+  //   access_token: '14109436.1677ed0.6766c773b72749588fcdefa86020f837',
+  //   count: 12
+  // };
+  
+  // $.ajax({
+  //   url: 'https://api.instagram.com/v1/self/media/recent',
+  //   data: parameters,
+  //   dataType: 'jsonp',
+  //   type: 'GET'
+  // })
+  // .done(function(result){ 
+  //   $.each(result.data, function(i, data) {
+  //     var instaPhoto = showInsta(data);
+      
+  //     if (i < 4) {
+  //       $('#insta-items').append(instaPhoto);
+  //     } else if (i < 8) {
+  //       $('#insta-items-2').append(instaPhoto);
+  //     } else if (i < 12) {
+  //       $('#insta-items-3').append(instaPhoto);
+  //     }
+
+  //   });
    
-  })
-  .fail(function(jqXHR, error){ 
-    let errorElem = showError(error);
-    $('#insta-items').append(errorElem);
-  });
+  // })
+  // .fail(function(jqXHR, textStatus, errorThrown){ 
+  //   alert (errorThrown);
+  //   // var errorElem = showError(error);
+  //   // $('#insta-items').append(errorElem);
+  // });
 }
 
 //Form Submission
@@ -57,10 +73,10 @@ function getInsta() {
 function submitForm(){
 
     // Initiate Variables With Form Content
-  let name = $('#name').val();
-  let email = $('#email').val();
-  let phone = $('#phone').val();
-  let message = $('#message').val();
+  var name = $('#name').val();
+  var email = $('#email').val();
+  var phone = $('#phone').val();
+  var message = $('#message').val();
     
   $.ajax({
     type: 'POST',
@@ -83,8 +99,8 @@ $(document).ready(function() {
   // Swap portfolio item in DOM
   $('.portfolio-items').on('click', '.portfolio-item', function() {
     event.preventDefault();
-    let hashTag = $(this).find('h2').text();
-    let project = $(this).find('a').attr('src');
+    var hashTag = $(this).find('h2').text();
+    var project = $(this).find('a').attr('src');
     // let webProject = 'pages/' + project;
       
     $('.hash-font').text(hashTag);
@@ -97,38 +113,38 @@ $(document).ready(function() {
 
     // Portfolio Hover Effect
 
-  if (Modernizr.touchevents) {
+  // if (Modernizr.touchevents) {
 
     // show the close overlay button
 
-    $('.close-overlay').removeClass('hidden');
+    // $('.close-overlay').removeClass('hidden');
 
     // handle the adding of hover class when clicked
-    $('#portfolio-img').click(function(e){
-      if (!$(this).hasClass('hover')) {
-        $(this).addClass('hover');
-      }
-    });
+    // $('#portfolio-img').click(function(){
+    //   if (!$(this).hasClass('hover')) {
+    //     $(this).addClass('hover');
+    //   }
+    // });
 
   // handle the closing of the overlay
 
-    $('.close-overlay').click(function(e){
-      e.preventDefault();
-      e.stopPropagation();
-      if ($(this).closest('.img').hasClass('hover')) {
-        $(this).closest('.img').removeClass('hover');
-      } else {
+    // $('.close-overlay').click(function(e){
+    //   e.preventDefault();
+    //   e.stopPropagation();
+    //   if ($(this).closest('.img').hasClass('hover')) {
+    //     $(this).closest('.img').removeClass('hover');
+    //   } else {
           // handle the mouseenter functionality
-        $('.portfolio-item').mouseenter(function(){
-          $(this).addClass('hover');
-        });
-        // handle the mouseleave functionality
-        $('.portfolio-item').mouseleave(function(){
-          $(this).removeClass('hover');
-        });
-      }
-    });
-  }
+  $('.portfolio-item').mouseenter(function(){
+    $(this).addClass('hover');
+  });
+  // handle the mouseleave functionality
+  $('.portfolio-item').mouseleave(function(){
+    $(this).removeClass('hover');
+  });
+      // }
+    // });
+// }  
 
     // Contact Form
     
@@ -142,7 +158,7 @@ $(document).ready(function() {
       name: {
         validators: {
           stringLength: {
-            min: 2,
+            min: 2
           },
           notEmpty: {
             message: 'Please supply your name'
